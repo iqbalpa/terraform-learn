d478ea923c3a1fabc4d92947663983235313f9ba
iqbalpa <iqbalpahlevi16@gmail.com>
2024-07-24 12:24:40 +0700

    feat: crete variables

--- main.tf
+++ main.tf
@@ -8,7 +8,10 @@ terraform {
 }
 
 provider "google" {
-  project = "movies-catalog-429305"
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
